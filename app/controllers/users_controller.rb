44fbf5503a5dab2fa3336b15b1c248888c939a63
yukashimizu <sdq.53.z@gmail.com>
2018-09-03 00:46:31 +0000

    sign up redirect to top

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "ユーザーを登録しました"
-      redirect_to @user
+      redirect_to root_url
     else
       flash[:danger] = "ユーザーの登録に失敗しました"
       render :new
