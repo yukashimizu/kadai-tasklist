84a2a227ab22fa46b9e15a222a278780aeb4ad79
yukashimizu <sdq.53.z@gmail.com>
2018-09-03 00:19:35 +0000

    add crud

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,13 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    if logged_in?
+      @user = current_user
+      @task = current_user.tasks.build
+      @tasks = current_user.tasks.order("created_at DESC").page(params[:page])
+    end
   end
   
   def show
@@ -13,7 +18,7 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "タスクが投稿されました"
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root to: "tasks#index"
+  
+  get "signup", to: "users#new"
+  resources :users, only: [:index, :show, :new, :create]
+  
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout",to: "sessions#destroy"
   
   resources :tasks
 end
